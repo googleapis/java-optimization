0a5ea560d12f1ef28928f26d7645206a99bfbd4e
Changsong Dong <changsongd@google.com>
2022-04-08 08:47:33 -0700

    reduce buckeet name length

--- samples/snippets/src/main/java/com/example/optimizationai/AsyncApi.java
+++ samples/snippets/src/main/java/com/example/optimizationai/AsyncApi.java
@@ -49,37 +49,37 @@ public class AsyncApi {
   }
 
   public static void callAsyncApi(
-    String projectParent,
-    String inputUri, 
-    String outputUri) throws Exception {
+      String projectParent,
+      String inputUri, 
+      String outputUri) throws Exception {
     GcsSource gcsSource = GcsSource.newBuilder().setUri(inputUri).build();
     InputConfig inputConfig = 
-      InputConfig.newBuilder()
-        .setGcsSource(gcsSource)
-        .setDataFormat(DataFormat.JSON)
-        .build();
+        InputConfig.newBuilder()
+          .setGcsSource(gcsSource)
+          .setDataFormat(DataFormat.JSON)
+          .build();
     GcsDestination gcsDestination = 
-      GcsDestination.newBuilder().setUri(outputUri).build();
+        GcsDestination.newBuilder().setUri(outputUri).build();
     OutputConfig outputConfig = 
-      OutputConfig.newBuilder()
-        .setGcsDestination(gcsDestination)
-        .setDataFormat(DataFormat.JSON)
-        .build();
+        OutputConfig.newBuilder()
+          .setGcsDestination(gcsDestination)
+          .setDataFormat(DataFormat.JSON)
+          .build();
 
     AsyncModelConfig asyncModelConfig = 
-      AsyncModelConfig.newBuilder()
-        .setInputConfig(inputConfig)
-        .setOutputConfig(outputConfig)
-        .build();
+        AsyncModelConfig.newBuilder()
+          .setInputConfig(inputConfig)
+          .setOutputConfig(outputConfig)
+          .build();
     BatchOptimizeToursRequest request = 
-      BatchOptimizeToursRequest.newBuilder()
-        .setParent(projectParent)
-        .addModelConfigs(asyncModelConfig)
-        .build();
+        BatchOptimizeToursRequest.newBuilder()
+          .setParent(projectParent)
+          .addModelConfigs(asyncModelConfig)
+          .build();
 
     FleetRoutingClient fleetRoutingClient = FleetRoutingClient.create();
     OperationFuture<BatchOptimizeToursResponse, AsyncModelMetadata> response = 
-      fleetRoutingClient.batchOptimizeToursAsync(request);
+        fleetRoutingClient.batchOptimizeToursAsync(request);
     System.out.format("the response name: %s\n", response.getInitialFuture().get().getName());
 
     // Block to wait for the job to finish.
